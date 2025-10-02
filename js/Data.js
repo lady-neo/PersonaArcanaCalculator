efd9ade8ce995d821255ebdeeebab6675f39288b
lady-neo <thaviny.to.use.games@gmail.com>
2025-10-02 19:56:56 -0300

    fixing path issue

--- js/Data.js
+++ js/Data.js
@@ -1,27 +1,99 @@
 const ARCANAS_DATA = {
-  0: { name: "The Fool", image_url: "images/tarot/0.png" },
-  1: { name: "The Magician", image_url: "images/tarot/1.png" },
-  2: { name: "The High Priestess", image_url: "images/tarot/2.png" },
-  3: { name: "The Empress", image_url: "images/tarot/3.png" },
-  4: { name: "The Emperor", image_url: "images/tarot/4.png" },
-  5: { name: "The Hierophant", image_url: "images/tarot/5.png" },
-  6: { name: "The Lovers", image_url: "images/tarot/6.png" },
-  7: { name: "The Chariot", image_url: "images/tarot/7.png" },
-  8: { name: "Justice", image_url: "images/tarot/8.png" },
-  9: { name: "The Hermit", image_url: "images/tarot/9.png" },
-  10: { name: "Wheel of Fortune", image_url: "images/tarot/10.png" },
-  11: { name: "Strength", image_url: "images/tarot/11.png" },
-  12: { name: "The Hanged Man", image_url: "images/tarot/12.png" },
-  13: { name: "Death", image_url: "images/tarot/13.png" },
-  14: { name: "Temperance", image_url: "images/tarot/14.png" },
-  15: { name: "The Devil", image_url: "images/tarot/15.png" },
-  16: { name: "The Tower", image_url: "images/tarot/16.png" },
-  17: { name: "The Star", image_url: "images/tarot/17.png" },
-  18: { name: "The Moon", image_url: "images/tarot/18.png" },
-  19: { name: "The Sun", image_url: "images/tarot/19.png" },
-  20: { name: "Judgement/Aeon", image_url: "images/tarot/20.png" },
-  21: { name: "The World", image_url: "images/tarot/21.png" },
-  99: { name: "Unknown/Unnumbered", image_url: "https://placehold.co/80x80/3b82f6/FFFFFF?text=" }
+  0: {
+    name: "The Fool",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759343551/Fool-0_vfvgvx.png"
+  },
+  1: {
+    name: "The Magician",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759344820/Magician-0_pobyez.png"
+  },
+  2: {
+    name: "The High Priestess",
+    image_url:
+      "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345696/Priestess-0_sy2zjb.png"
+  },
+  3: {
+    name: "The Empress",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345648/Empress-0_wzgoks.png"
+  },
+  4: {
+    name: "The Emperor",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345647/Emperor-0_ukzupp.png"
+  },
+  5: {
+    name: "The Hierophant",
+    image_url:
+      "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345646/Hierophant-0_yp8gpy.png"
+  },
+  6: {
+    name: "The Lovers",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345646/Lovers-0_t3ozk9.png"
+  },
+  7: {
+    name: "The Chariot",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345642/Chariot-0_l5b0b2.png"
+  },
+  8: {
+    name: "Justice",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345640/Justice-0_xv6z3s.png"
+  },
+  9: {
+    name: "The Hermit",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345639/Hermit-0_wf5k2p.png"
+  },
+  10: {
+    name: "Wheel of Fortune",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345640/Fortune-0_pyyplw.png"
+  },
+  11: {
+    name: "Strength",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345635/Strength-0_bjvvde.png"
+  },
+  12: {
+    name: "The Hanged Man",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345633/Hanged_Man_yyeur4.png"
+  },
+  13: {
+    name: "Death",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345631/Death-0_mgdfxe.png"
+  },
+  14: {
+    name: "Temperance",
+    image_url:
+      "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345630/Temperance-0_otxm0l.png"
+  },
+  15: {
+    name: "The Devil",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759346552/Devil-0_qrkfxa.png"
+  },
+  16: {
+    name: "The Tower",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345630/Tower-0_hljbnv.png"
+  },
+  17: {
+    name: "The Star",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345626/Star-0_y5nqu7.png"
+  },
+  18: {
+    name: "The Moon",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759346272/Moon-0_olozdx.png"
+  },
+  19: {
+    name: "The Sun",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345626/Sun-0_ozjk8h.png"
+  },
+  20: {
+    name: "Judgement/Aeon",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345625/Judgement_e0eoag.png"
+  },
+  21: {
+    name: "The World",
+    image_url: "https://res.cloudinary.com/dhbrdf1p8/image/upload/v1759345625/World-0_wknbcy.png"
+  },
+  99: {
+    name: "Unknown/Unnumbered",
+    image_url: "https://placehold.co/80x80/3b82f6/FFFFFF?text=?"
+  }
 };
 
 const CHARACTERS_DATA = [
